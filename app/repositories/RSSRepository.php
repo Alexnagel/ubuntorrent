8782129ca4eac002b7ba0161e446339f8ac65477
Alexnagel <alex_na@outlook.com>
2013-11-04 21:34:51 +0100

    checking

--- app/repositories/RSSRepository.php
+++ app/repositories/RSSRepository.php
@@ -65,7 +65,7 @@ class RSSRepository implements RepositoryInterface
 		{
 			$item_date = strtotime($item->pubDate);
 			echo $item_date . "\n";
-			if($item_date >= $last_str && $item_date <= $current_day)
+			if($item_date > $last_str && $item_date < $current_day)
 			{
 				echo $item_date . "\n";
 				$item_arr 			= $this->regexTorrentItem($item->title);
