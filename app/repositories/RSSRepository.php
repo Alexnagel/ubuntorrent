fdb61e615b49239309621d628989d47abb0674b0
Alexnagel <alex_na@outlook.com>
2013-11-04 21:30:15 +0100

    checking

--- app/repositories/RSSRepository.php
+++ app/repositories/RSSRepository.php
@@ -64,7 +64,7 @@ class RSSRepository implements RepositoryInterface
 		foreach($feed_items as $item)
 		{
 			$item_date = strtotime($item->pubDate);
-
+			echo $item_date . "\n";
 			if($item_date >= $last_str && $item_date <= $current_day)
 			{
 				echo $item_date . "\n";
