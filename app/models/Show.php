e190d36a5339456e5e0bc6205151c15575898ac7
Alexnagel <alex_na@outlook.com>
2013-12-03 23:45:22 +0100

    show changes

--- app/models/Show.php
+++ app/models/Show.php
@@ -15,10 +15,21 @@ class Show {
 		$this->name			= $values[0];
 		$this->image 		= $values[1];
 		$this->status		= $values[2];
+		$this->statusLabel 	= $this->setStatusLabel($values[2]);
 		$this->genre		= $values[3];
 		$this->summary		= $values[4];
 		$this->cast			= $values[5];
 		$this->release_date	= $values[6];
 		$this->air_date		= $values[7];
+	}
+
+	private function setStatusLabel($status)
+	{
+		switch (strtolower($status)) {
+			case 'continuing': 	$statusClass = "success"; 	break;
+			case 'ended': 		$statusClass = "danger"; 	break;
+			default: 			$statusClass = "warning"; 	break;
+		}
+		return $statusClass;
 	}
 }
